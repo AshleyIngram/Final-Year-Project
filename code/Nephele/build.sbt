5591fc183c74ae55fcadbecf19e58cc12a44c244
Ashley Ingram <ashley2504@hotmail.com>
2014-04-19 16:09:05 +0100

    Adding Nephele project and building ReverseLinkGraph.

---
+++ code/Nephele/build.sbt
@@ -0,0 +1,7 @@
+name := "Nephele"
+
+version := "1.0"
+
+libraryDependencies += "eu.stratosphere" % "stratosphere-clients" % "0.4"
+
+libraryDependencies += "eu.stratosphere" % "stratosphere-scala" % "0.4"
---
+++ code/Nephele/src/main/scala/ReverseLinkGraph.scala
@@ -0,0 +1,30 @@
+/**
+ * Find the Reverse Link Graph using Nephele/Stratosphere
+ */
+import eu.stratosphere.client.{RemoteExecutor, LocalExecutor}
+
+import eu.stratosphere.api.scala._
+import eu.stratosphere.api.scala.operators._
+
+import AshleyIngram.FYP.Core
+
+object ReverseLinkGraph {
+  def main(args: Array[String]) {
+    val in = args(0)
+    val out = args(1)
+    val jobManagerIp = args(2)
+    val jobManagerPort = args(3).toInt
+    val jarFile = args(4)
+
+    // val input = DataSource(in, BinarySerializedInputFormat[(String, String)]())
+    val input = DataSource(in, CsvInputFormat[(String, String)]())
+    val links = input.flatMap(i => Core.ReverseLinkGraph.map(i._1, i._2))
+    val result = links.groupBy { case(link, _) => link }.reduce { (l1, l2) => (l1._1, l1._2 + ", " + l2._2) }
+
+    val output = result.write(out, CsvOutputFormat())
+    val plan = new ScalaPlan(Seq(output))
+
+    val executor = new RemoteExecutor(jobManagerIp, jobManagerPort, jarFile)
+    executor.executePlan(plan)
+  }
+}
